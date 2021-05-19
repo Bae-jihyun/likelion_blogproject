b6df2ba2252b32d9f1e0b7a2407fb1997505723f
Bae-jihyun <govl6113@naver.com>
2021-05-19 21:21:16 +0900

    intro수정

--- myvenv/Scripts/activate.bat
+++ myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ey611\OneDrive\바탕 화면\blogproject\myvenv
+set VIRTUAL_ENV=C:\Users\ey611\OneDrive\바탕 화면\likelion_blogproject\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- myvenv/Scripts/django-admin.py
+++ myvenv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!c:\users\ey611\onedrive\바탕 화면\blogproject\myvenv\scripts\python.exe
+#!c:\users\ey611\onedrive\바탕 화면\likelion_blogproject\myvenv\scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
